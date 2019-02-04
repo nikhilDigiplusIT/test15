effe8b1e8be21dee03407ff90909df601f53b4c8
Nikhil <nikhilj@digiplusit.com>
2019-02-04 12:41:04 +0530

    push

---
+++ www/MathCalculator.js
@@ -0,0 +1,13 @@
+var exec = require('cordova/exec');
+
+module.exports.add = function (arg0, success, error) {
+    exec(success, error, 'MathCalculator', 'add', [arg0]);
+};
+
+module.exports.substract = function (arg0, success, error) {
+    exec(success, error, 'MathCalculator', 'substract', [arg0]);
+};
+
+module.exports.getAppSignatures = function (arg0, success, error) {
+    exec(success, error, 'MathCalculator', 'getAppSignatures', [arg0]);
+};
